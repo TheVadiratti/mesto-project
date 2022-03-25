6854ac0cef3f81cf58cbfd8d0b37ae90acf82b93
Vadim Belousov <thevadiratti@yandex.ru>
2022-03-25 18:10:25 +0500

    add js code: text deletion input

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
+let input = document.querySelectorAll('.popup__input');
 let likeButtons = document.querySelector('.content__like-button');
+console.log(input);
 
 // editWindow
 
 function editWindowToggle() {
   popup.classList.toggle('popup_opened');
+  input[0].value = '';
+  input[1].value = '';
 };
 editButton.addEventListener('click', editWindowToggle);
 closeButton.addEventListener('click', editWindowToggle);

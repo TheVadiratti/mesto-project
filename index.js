ab022e7f0f502ee4196c40458498b401efa363f4
Vadim Belousov <thevadiratti@yandex.ru>
2022-04-05 17:59:36 +0500

    added editing the profile

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
+let profileName = document.querySelector('.profile__name');
+let profileDescription = document.querySelector('.profile__description');
+let popupForm = document.querySelector('.popup__form');
 let popup = document.querySelector('.popup');
-let editButton = document.querySelector('.profile__edit-button');
-let closeButton = document.querySelector('.popup__close-button');
 let input = document.querySelectorAll('.popup__input');
 
 // editWindow
 
 function editWindowToggle() {
   popup.classList.toggle('popup_opened');
-  input[0].value = '';
-  input[1].value = '';
+  input[0].value = profileName.textContent;
+  input[1].value = profileDescription.textContent;
 };
+let editButton = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.popup__close-button');
 editButton.addEventListener('click', editWindowToggle);
 closeButton.addEventListener('click', editWindowToggle);
 
+// formSubmit
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileName.textContent = input[0].value;
+  profileDescription.textContent = input[1].value;
+  editWindowToggle();
+}
+
+popupForm.addEventListener('submit', formSubmitHandler);
+
 // like
-let likeButtons = document.querySelectorAll('.content__like-button');
-for (let i = 0; i < likeButtons.length; i++) {
-  likeButtons[i].addEventListener('click', function (event) {
+const likeButtons = document.querySelectorAll('.content__like-button');
+likeButtons.forEach(function (item) {
+  item.addEventListener('click', function (event) {
     event.target.classList.toggle('content__like-button_active');
-    console.log(event.target);
   })
-};
+});
+
+const test = document.querySelector('.content__image');
+console.log(test.src);
